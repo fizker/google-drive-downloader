e0c4b76ae1ee95e740484975707d35751cc2e3c0
Benjamin Horsleben <benjamin@fizkerinc.dk>
2015-06-04 20:01:31 +0200

    The first 1000 children on the first level is shown.
    - The modified-date is properly filtered.

--- src/commands/list-children.js
+++ src/commands/list-children.js
@@ -3,4 +3,21 @@ var findItem = require('../find-item')
 
 module.exports = function(path, options) {
 	return findItem(path)
+		.then(parent => get(`files?q='${parent.id}' in parents&maxResults=1000`))
+		.then(res => res.items)
+		.then(options.modifiedAfter && (items => items
+			.filter(item => item.modifiedDate > options.modifiedAfter)
+		))
+		.then(children => children.map(child => {
+			var { mimeType, title, modifiedDate } = child
+			var path = title
+			if(mimeType == 'application/vnd.google-apps.folder') {
+				path += '/'
+			}
+			return {
+				path: path,
+				mimeType,
+				modifiedDate,
+			}
+		}))
 }
