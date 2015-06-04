90ed548abff1e65534b1040d0767105e016802f4
Benjamin Horsleben <benjamin@fizkerinc.dk>
2015-06-04 18:09:26 +0200

    cli have the currently wanted options.

--- cli.js
+++ cli.js
@@ -1,18 +1,30 @@
 require("babel/register")
 
-var yargs = require('yargs')
-
 var verifyDate = require('./src/verify-date')
 
-var args = yargs
-	.option('modified-after', {
-		alias: 'm',
-		describe: 'json date, it will only return matches modified after this date',
-	})
-	.argv
+var yargs = require('yargs')
+	.usage('$0 <command>')
+	.command('list-children', 'List all files within a given folder', listChildrenCommand)
+	.help('help')
+	.required(1, '')
 
-var opts = {
-	modifiedAfter: verifyDate(args.modifiedAfter)
+yargs.argv
+
+function listChildrenCommand(yargs) {
+	var args = yargs
+		.usage('$0 list-children <path>')
+		.required(2, 'The path is required. It is case-sensitive. Use `/` for the root.')
+		.option('modified-after', {
+			alias: 'm',
+			describe: 'json date, it will only return matches modified after this date',
+		})
+		.help('help')
+		.argv
+
+	var path = args._[1]
+	var opts = {
+		modifiedAfter: verifyDate(args.modifiedAfter),
+	}
+
+	console.log(path, opts)
 }
-
-console.log(opts)
