fedb001666f06adb3e293a46471434eae1cb2a66
Benjamin Horsleben <benjamin@fizkerinc.dk>
2015-06-05 10:38:32 +0200

    The folder to download to will now be created before downloading

--- src/commands/get-files.js
+++ src/commands/get-files.js
@@ -1,10 +1,14 @@
-var get = require('../get')
+var mkdirp = require('mkdirp')
 var { findItem, listFiles, downloadFile } = require('../drive')
 
 module.exports = function(path, options) {
 	var outputDir = options.outputDir || process.cwd()
-	return findItem(path)
-		.then(file => listFiles(file, options))
+	return Promise.all([
+		findItem(path)
+			.then(file => listFiles(file, options)),
+		new Promise((resolve, reject)=>mkdirp(outputDir, err => err ? reject(err) : resolve())),
+	])
+		.then(x => x[0])
 		.then(files => files.filter(file => file.mimeType != 'application/vnd.google-apps.folder'))
 		.then(files => Promise.all(
 			files.map(file => downloadFile(file, outputDir))
